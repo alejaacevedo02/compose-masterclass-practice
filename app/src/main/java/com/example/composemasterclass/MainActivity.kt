b885690d0deae485537c1b39fe453b70c92963b3
Alejandra ACEVEDO <alejandra.acevedourrego@worldline.com>
2026-01-28 10:50:50 +0100

    feat: do a customLazyLayout

--- app/src/main/java/com/example/composemasterclass/MainActivity.kt
+++ app/src/main/java/com/example/composemasterclass/MainActivity.kt
@@ -4,32 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeGestures
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.composemasterclass.basic_modifiers.FocusManagementModifiersDemo
-import com.example.composemasterclass.measurements.SubcomposePagedRow
+import androidx.compose.ui.unit.IntOffset
+import com.example.composemasterclass.measurements.LazyMindMap
+import com.example.composemasterclass.measurements.MindMapItem
 import com.example.composemasterclass.ui.theme.ComposemasterclassTheme
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,37 +33,51 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     contentWindowInsets = WindowInsets.safeGestures
                 ) { innerPadding ->
-//                    SpacingModifierDemo(
-//                        modifier = Modifier
-//                            .padding(innerPadding)
-//                            .consumeWindowInsets(innerPadding)
-//                    )
-                    //NumberGuessScreenRoot(modifier = Modifier.padding(innerPadding))
-                    //    FocusManagementModifiersDemo(modifier = Modifier.padding(innerPadding))
-
-                    var page by remember {
-                        mutableIntStateOf(0)
-                    }
-                    Column(Modifier.padding(innerPadding)) {
-                        SubcomposePagedRow(
-                            page = page,
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            (1..1000).forEach { _ ->
-                                Box(
-                                    modifier = Modifier
-                                        .height(100.dp)
-                                        .width(Random.nextInt(300).dp)
-                                        .background(Color(Random.nextInt()))
+                    val mindMapItems = remember {
+                        listOf(
+                            MindMapItem(
+                                title = "Hello world 1",
+                                percentageOffset = Offset(
+                                    0f,
+                                    0f
                                 )
-                            }
-                        }
-                        Button(onClick = {
-                            page++
-                        }) {
-                            Text("Next Page")
-                        }
+                            ),
+                            MindMapItem(
+                                title = "Hello world 2",
+                                percentageOffset = Offset(
+                                    1f,
+                                    0f
+                                )
+                            ),
+                            MindMapItem(
+                                title = "Hello world 3",
+                                percentageOffset = Offset(
+                                    0.3f,
+                                    -0.5f
+                                )
+                            ),
+                            MindMapItem(
+                                title = "Hello world 4",
+                                percentageOffset = Offset(
+                                    -0.2f,
+                                    0.5f
+                                )
+                            )
+                        )
                     }
+                    var mindMapOffset by remember {
+                        mutableStateOf(IntOffset.Zero)
+                    }
+                    LazyMindMap(
+                        itemsList = mindMapItems,
+                        mindMapOffset = mindMapOffset,
+                        onDrag = { delta -> mindMapOffset += delta },
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+
+
+                    )
                 }
             }
         }
