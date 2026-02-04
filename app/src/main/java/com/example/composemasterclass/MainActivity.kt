b6faa9024802798ed9644b140bcd2c45c1dcbdad
Alejandra ACEVEDO <alejandra.acevedourrego@worldline.com>
2026-02-04 12:19:52 +0100

    feat: use ViewModel for the sideEffects
    - we can even manage the snackBarHostState from VM
    - remove LaunchedEffect except for permission management

--- app/src/main/java/com/example/composemasterclass/MainActivity.kt
+++ app/src/main/java/com/example/composemasterclass/MainActivity.kt
@@ -8,22 +8,11 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeGestures
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
-import com.example.composemasterclass.measurements.LazyMindMap
-import com.example.composemasterclass.measurements.MindMapItem
-import com.example.composemasterclass.side_effects.DisposableEffectDemo
 import com.example.composemasterclass.side_effects.LaunchedEffectDemo
 import com.example.composemasterclass.ui.theme.ComposemasterclassTheme
 
@@ -37,24 +26,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     contentWindowInsets = WindowInsets.safeGestures
                 ) { innerPadding ->
-                    var toggle by remember {
-                        mutableStateOf(false)
-                    }
-                    if (toggle) {
-                        Text("DispossableEffect Demo", modifier = Modifier.padding(innerPadding))
-                        DisposableEffectDemo(modifier = Modifier.padding(innerPadding))
-                    }
-                    Button(
-                        onClick = {
-                            toggle = !toggle
-                        },
+                    LaunchedEffectDemo(
                         modifier = Modifier
                             .fillMaxSize()
-                            .wrapContentSize()
                             .padding(innerPadding)
-                    ) {
-                        Text(text = "Toggle Demo")
-                    }
+                    )
                 }
             }
         }
